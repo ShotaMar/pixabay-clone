ad05a94e140fe39c778dffafe8233ed42eb53e06
ShotaMar <nu05kyntobronk@gmail.com>
2022-06-13 21:53:20 +0900

    fix all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageGallery from './ImageGallery';
+import { useRef, useState } from 'react';
 
 function App() {
+  const APIKEY = process.env.React_APP_API_KEY;
+  const [fetchData, setFetchData] = useState([])
+  const ref = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const endpointURL = `https://pixabay.com/api/?key=${APIKEY}&q=${ref.current.value}&image_type=photo`
+    fetch(endpointURL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setFetchData(data.hits)
+        console.log(data.hits)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container'>
+      <h2>My Pixabay</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='画像を探す' ref={ref}/>
+      </form>
+      <ImageGallery fetchData={fetchData}/>
+    </div>)
 }
 
 export default App;
